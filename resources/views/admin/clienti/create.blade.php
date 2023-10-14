878da3cc4503bde60e6e8716fad5fc897db46ad8
fbisceglia02 <ferruccio3214@gmail.com>
2023-10-14 16:52:52 +0200

    feat: create create view and return it in create controller

---
+++ resources/views/admin/clienti/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-8">
+            <div class="card">
+                <div class="card-body">
+                    <form action="" method="POST">
+
+
+                        <div class="places-avaiable d-flex flex-column">
+                            <label for="posti_disponibili">Elenco Corsisti</label>
+                            <select name="" id="">
+                                <option value=""></option>
+                            </select>
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+@endsection
