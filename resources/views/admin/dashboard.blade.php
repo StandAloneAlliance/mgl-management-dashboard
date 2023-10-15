97a947fd66202ebbbaad6320f100e11b68663b14
antoniogisondi <anto.gisondi@tiscali.it>
2023-10-15 20:49:24 +0200

    [FEAT] Create sidebar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,6 +5,33 @@
     <div class="spinner"></div>
 </div>
 
+<nav class="sidebar">
+    <div class="sidebar-top">
+        <a href="#" class="logo_wrapper">
+            Inserisci Immagine
+        </a>
+    </div>
+
+    <div class="sidebar-links">
+        <ul>
+            <li>
+                <a href="{{ route('admin.customers.index')}}" title="Corsisti" class="tooltip">
+                    <img src="{{ asset('images/customer.svg') }}" alt="">
+                    <span class="link hide">Home</span>
+                    <span class="tooltip_content">Home</span>
+                </a>
+            </li>
+            {{-- other sidebar links --}}
+        </ul>
+    </div>
+    <div class="expand-btn">
+        {{-- insert image --}}
+        <span class="hide">
+            Collapse
+        </span>
+    </div>
+</nav>
+
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
         MGL Management Dashboard
@@ -22,7 +49,7 @@
 
                    {{-- <a href="{{ route('admin.courses.index')}}" class="btn btn-primary mt-3">Guarda la pagina dei corsi</a> --}}
 
-                   <a href="{{ route('admin.customers.index')}}" class="btn btn-primary mt-3">Guarda la pagina dei corsisti</a>
+                   
                    {{-- <a href="{{ route('admin.courses.index')}}">Clicca qua per vedere corsis</a> --}}
                    {{-- {!! $htmlContent !!} --}}
                 </div>
@@ -31,8 +58,8 @@
     </div>
 </div>
 
-
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanillajs@1.0/dest/cjs/index.min.js"></script>
     <script>
         $(document).ready(function () {
             $('#bash-form').submit(function (event) { // Listen for form submission
@@ -68,5 +95,32 @@
             })
 
 
+    </script>
+    
+    <script>
+        const expand_btn = document.querySelector(".expand-btn");
+        let activeIndex;
+
+        expand_btn.addEventListener("click", () =>{
+            document.body.classList.toggle("collapsed")
+        });
+
+        const current = window.location.href;
+
+        const allLinks = document.querySelectorAll(".sidebar-links a");
+
+        allLinks.forEach((elem) => {
+            elem.addEventListener('click', function(){
+                const hrefLinkClick = elem.href;
+
+                allLinks.forEach((link) => {
+                    if(link.href == hrefLinkClick){
+                        link.classList.add("active")
+                    } else{
+                        link.classList.remove('active')
+                    }
+                })
+            })
+        });
     </script>
 @endsection
