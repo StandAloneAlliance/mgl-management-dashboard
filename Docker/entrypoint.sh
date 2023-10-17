54c82a6a28e174696bae0af2fca315f60ed61196
fbisceglia02 <ferruccio3214@gmail.com>
2023-10-17 21:31:45 +0200

    feat: add Docker, docker-compose.yml and Docker/entrypoint.sh

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#set workdir
+cd /var/www
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction 
+fi 
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo "env file exists"
+fi
+
+php artisan migrate
+php artisan key:generate
+npm install esbuild-linux-64
+php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+exec docker-php-entrypoint
