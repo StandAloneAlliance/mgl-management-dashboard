f513952a7c0efeae037a3bbf1e4f5d1c60bbf7a1
antoniogisondi <anto.gisondi@tiscali.it>
2023-10-18 09:53:17 +0200

    [FIX] Task scheduler set on expiry date and works

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('courses:update-status')->everyFiveMinutes();
         // $schedule->command('inspire')->hourly();
     }
 
