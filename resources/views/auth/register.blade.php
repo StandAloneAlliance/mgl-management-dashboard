7fb07210eb40aa274371fc2a29f6a65e2d82d749
antoniogisondi <anto.gisondi@tiscali.it>
2023-10-18 20:37:28 +0200

    [FEAT] Implement password strong control

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,8 +57,11 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                                <input spellcheck="false" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" onkeyup="handleChange()">
+                                <div id="bars">
+                                    <div></div>
+                                </div>
+                                <div id="strength"></div>
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -88,4 +91,63 @@
         </div>
     </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/vanillajs@1.0.1/dest/cjs/index.min.js"></script>
+<script>
+    const bars = document.querySelector("#bars")
+    let strengthDiv = document.querySelector("#strength")
+    let passwordInput = document.querySelector("#password")
+
+    const strength = {
+        1: "weak",
+        2: "medium",
+        3: "strong"
+    };
+
+    const getIndicator = (password, value) => {
+        for (let i = 0; i < password.length; i++) {
+            let char = password.charCodeAt(i);
+            if (!value.upper && char >= 65 && char <= 90) {
+                value.upper = true
+            } else if (value.numbers && char >= 48 && char <= 57){
+                value.numbers = true
+            } else if (!value.lower && char >= 97 && char <= 122){
+                value.lower = true
+            }
+        }
+
+        let indicator = 0
+        for (let metric in value) {
+            if (value[metric] === true) {
+                indicator++
+            }
+        }
+
+        return strength[indicator] ?? "";
+    }
+
+    const getStrength = (password) => {
+        let strengthValue = {
+            upper : false,
+            numbers : false,
+            lower: false,
+        };
+
+        return getIndicator (password, strengthValue);
+    };
+
+    const handleChange = () => {
+        let { value: password } = passwordInput
+        console.log(password)
+        const strengthText = getStrength(password)
+        bars.classList = ""
+
+        if (strengthText) {
+            strengthDiv.innerText = `${strengthText} password`
+            bars.classList.add(strengthText)
+        } else {
+            strengthDiv.innerText = ""
+        }
+    }
+
+</script>
 @endsection
