0f0f05841f7dd073f80fbb9378e246b966decff9
antoniogisondi <anto.gisondi@tiscali.it>
2023-10-20 12:16:14 +0200

    [FEAT] Create relation between users and customers

--- app/Console/Commands/UpdateCourseStatus.php
+++ app/Console/Commands/UpdateCourseStatus.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Course;
+use App\Models\Customer;
+use App\Models\User;
 use Carbon\Carbon;
 
 class UpdateCourseStatus extends Command
@@ -39,6 +41,18 @@ class UpdateCourseStatus extends Command
         }
 
         $this->info('Course statuses updated successfully.');
+
+        $expiry_courses = Course::whereDate('data_scadenza', Carbon::now()->subDays(8))->get();
+
+        foreach ($expiry_courses as $course) {
+            foreach ($course->customers as $customer) {
+                $user = $customer->user;
+
+                if ($user) {
+                    // Invia la notifica all'utente
+                }
+            }
+        }
     }
 }
 
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,15 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Course;
+use App\Models\User;
 
 class Customer extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'surname', 'cfr', 'email', 'cover_image', 'date_of_birth', 'city_of_birth', 'task'];
+    protected $fillable = ['user_id', 'name', 'surname', 'cfr', 'email', 'cover_image', 'date_of_birth', 'city_of_birth', 'task'];
 
     public function courses()
     {
         return $this->belongsToMany(Course::class, 'course_customer');
     }
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
 }
