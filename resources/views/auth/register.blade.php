e13f62fd193dcdd049fdb60643b306f8807eff26
antoniogisondi <anto.gisondi@tiscali.it>
2023-11-20 17:07:31 +0100

    [FIX] Responsive register form to fixed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,69 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container-register">
-    <div class="register-card">
-        <h2>Registrati</h2>
-        <h3>Inserisci le tue credenziali</h3>
-        <form method="POST" action="{{ route('register') }}" class="register-form">
-        @csrf
-            <div class="mb-2 form-group d-flex justify-content-start flex-wrap">
-                <label for="name" class="col-form-label">Inserisci il nome *</label>
-                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Inserisci il nome">
-                @error('name')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-
-            <div class="mb-2 form-group d-flex justify-content-start flex-wrap">
-                <label for="surname" class="col-form-label">Inserisci il cognome *</label>
-                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus placeholder="Inserisci il cognome">
-                @error('surname')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-
-            <div class="mb-2 form-group d-flex justify-content-start flex-wrap">
-                <label for="email" class="col-form-label">Inserisci l'email *</label>
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Inserisci l'email">
-                @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-
-            <div class="form-group d-flex justify-content-start flex-wrap">
-                <label for="password" class="col-form-label">Inserisci la password *</label>
-                <input spellcheck="false" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" onkeyup="handleChange()" autofocus placeholder="Inserisci la passsword">
-                <div id="bars">
-                    <div></div>
+<div class="container-fluid bg-container-register">
+    <div class="container-register mt-4">
+        <div class="row register-card">
+            <h2>Registrati</h2>
+            <h3>Inserisci le tue credenziali</h3>
+            <form method="POST" action="{{ route('register') }}" class="col-lg-8 col-md-6 register-form">
+            @csrf
+                <div class="mb-2 form-group d-flex justify-content-start flex-wrap">
+                    <label for="name" class="col-form-label">Inserisci il nome *</label>
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Inserisci il nome">
+                    @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
-                <div id="strength"></div>
-                @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-
-            <div class="mb-2 form-group d-flex justify-content-start flex-wrap">
-                <label for="password-confirm" class="col-form-label">Conferma la password*</label>
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" autofocus placeholder="Ripeti la password">           
-            </div>     
-
-            <button type="submit" class="btn-register">
-                {{ __('Registrati') }}
-            </button>
-        </form>
+    
+                <div class="mb-2 form-group d-flex justify-content-start flex-wrap">
+                    <label for="surname" class="col-form-label">Inserisci il cognome *</label>
+                    <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus placeholder="Inserisci il cognome">
+                    @error('surname')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+    
+                <div class="mb-2 form-group d-flex justify-content-start flex-wrap">
+                    <label for="email" class="col-form-label">Inserisci l'email *</label>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Inserisci l'email">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+    
+                <div class="form-group d-flex justify-content-start flex-wrap">
+                    <label for="password" class="col-form-label">Inserisci la password *</label>
+                    <input spellcheck="false" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" onkeyup="handleChange()" autofocus placeholder="Inserisci la passsword">
+                    <div id="bars">
+                        <div></div>
+                    </div>
+                    <div id="strength"></div>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+    
+                <div class="mb-2 form-group d-flex justify-content-start flex-wrap">
+                    <label for="password-confirm" class="col-form-label">Conferma la password*</label>
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" autofocus placeholder="Ripeti la password">           
+                </div>     
+    
+                <button type="submit" class="btn-register">
+                    {{ __('Registrati') }}
+                </button>
+            </form>
+        </div>
     </div>
+    
 </div>
 <script src="https://cdn.jsdelivr.net/npm/vanillajs@1.0.1/dest/cjs/index.min.js"></script>
 <script>
